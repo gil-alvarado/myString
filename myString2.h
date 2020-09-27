d58d0b1eb7e9232be7342e777543beb87e48f6f6
userGil <49108042+userGil@users.noreply.github.com>
2020-09-27 16:34:23 -0400

    add stirng2 to project (WIP)

---
+++ myString2.h
@@ -0,0 +1,41 @@
+	/*
+ * myString2.h
+ *      Assignment 2
+ */
+//NOTE THIS IS INCOMPLETE
+#ifndef MYSTRING2_H_
+#define MYSTRING2_H_
+
+#include "myString.h"
+
+using namespace::notstd;
+
+class stringV2
+{
+	public:
+
+	stringV2();
+	stringV2(char*);
+	~stringV2();
+
+	size_t rSearchChar(const char&, const char*);
+	size_t rSearchMyString(const myString&);
+
+	size_t countChar(const char&);
+	size_t countMyString(const myString&);
+					//LOCATION, LENGTH, CHAR
+	void replaceChar(char *, char, const char&);
+
+	//void resize(const size_t newSize);
+	//INCOMPLETE
+	char* begin();
+	char* end();
+	char* next();
+
+	private:
+	char data*;
+	size_t aSize;
+	size_t length;
+
+};
+#endif /* MYSTRING2_H_ */
